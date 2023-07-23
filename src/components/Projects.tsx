40a9aa222e3b667153417900433e1b6495c9a872
Sai Sawant <saisawant2003@gmail.com>
2023-07-23 21:20:36 +0530

    style fixes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -49,8 +49,8 @@ function Project({ title, description, tags, imageUrl }: ProjectsProps) {
       transition={{ ease: "easeOut" }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className=" bg-gray-100 max-w-[42rem] sm:h-[20rem] border border-black/5 overflow-hidden sm:pr-8 relative  even:pl-8 hover:bg-gray-200 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="py-4 pb-7  px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <section className="  bg-gray-100 max-w-[42rem] sm:h-[20rem] border border-black/5 overflow-hidden sm:pr-8 relative  even:pl-8 hover:bg-gray-200 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="py-4 pb-7  px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -65,7 +65,7 @@ function Project({ title, description, tags, imageUrl }: ProjectsProps) {
           </ul>
         </div>
         <Image
-          className="absolute top-8 -right-40
+          className="absolute hidden sm:block top-8 -right-40
         w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] 
         transition-all
         group-even:-left-40
