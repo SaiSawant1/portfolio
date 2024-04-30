81f1cc5a9d7ce32efb13a10f7d3f93f875bc23ad
SaiSawant1 <saisawant2003@gmail.com>
2024-04-30 23:37:22 +0530

    updated portfolio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,29 +4,34 @@ import { useActiveSectionContext } from "@/context/ActiveSectionContext";
 import { projectsData } from "@/lib/data";
 import { useScroll, motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { useInView } from "react-intersection-observer";
 export default function Projects() {
-
   const { setActive } = useActiveSectionContext();
 
   const { ref, inView } = useInView({
     threshold: 0.5,
-  })
+  });
   React.useEffect(() => {
     if (inView) {
       setActive("Projects");
     }
-  },[inView]);
-
+  }, [inView]);
 
   return (
-    <section ref={ref} id="projects" className="text-center mb-28 scroll-mt-28 ">
+    <section
+      ref={ref}
+      id="projects"
+      className="text-center mb-28 scroll-mt-28 "
+    >
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((projects, index) => (
           <React.Fragment key={index}>
-            <Project key={index} {...projects} />
+            <Link href={projects.link}>
+              <Project key={index} {...projects} />
+            </Link>
           </React.Fragment>
         ))}
       </div>
@@ -52,7 +57,9 @@ function Project({ title, description, tags, imageUrl }: ProjectsProps) {
       <section className="  bg-gray-100 max-w-[42rem] sm:h-[20rem] border border-black/5 overflow-hidden sm:pr-8 relative  even:pl-8 hover:bg-gray-200 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="py-4 pb-7  px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+            {description}
+          </p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,8 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import carShowCase from "../../public/carShowCase.png";
 import BreadItClone from "../../public/BreadItClone.png";
+import ChatMate from "../../public/chat-mates-image.png";
 import TodoApp from "../../public/TodoApp.png";
 
 export const links = [
@@ -37,8 +36,7 @@ export const experiencesData = [
   {
     title: "Completed Higher Secondary Certificate",
     location: "Umbergaon, Gujrat",
-    description:
-      "Completed my HSC from SSV Gyan Kendra.",
+    description: "Completed my HSC from SSV Gyan Kendra.",
     icon: React.createElement(LuGraduationCap),
     date: "2021",
   },
@@ -54,33 +52,52 @@ export const experiencesData = [
     title: "SDE Intern",
     location: "Pune, Maharashtra",
     description:
-      "I'm Worked as an SDE intern at Elite Bridges. As a inter i was tasked with developing a windows application for the company.",
+      "I Worked as an SDE intern at Elite Bridge. As am intern I was tasked with developing a windows application for the company.",
     icon: React.createElement(CgWorkAlt),
-    date: "2023 - present",
+    date: "2023 - 2023",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "Car Hub",
+    title: "Chat Mate",
     description:
-      "Car hub is a front end web application build to demonstrate front-end skills and working API on next js",
-    tags: ["React", "Next.js", "Tailwind", "Figma"],
-    imageUrl: carShowCase,
+      "Chat Mate is instant messaging, voice and video call enbaling social media platform It allows user to create servers(communities) and and allows community members to communitcate",
+    tags: [
+      "React",
+      "Next.js",
+      "Tailwind",
+      "Figma",
+      "WebSockets",
+      "Prisma",
+      "PostgreSQL",
+    ],
+    imageUrl: ChatMate,
+    link: "https://chat-mates-six.vercel.app/",
   },
   {
     title: "BreadIt",
     description:
       "Breadit is full stack responsive web application that allows user to create communities and post various kind of media to it. It has complete CURD functionalities for users.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "MYSql","Next-Auth","Prisma"],
+    tags: [
+      "React",
+      "TypeScript",
+      "Next.js",
+      "Tailwind",
+      "MYSql",
+      "Next-Auth",
+      "Prisma",
+    ],
     imageUrl: BreadItClone,
+    link: "https://breadit-beige.vercel.app/",
   },
   {
     title: "Todo App",
     description:
       "A simple todo list app that alow users to add, delete filter task it has also users to switch between mark mode and light mode.",
-    tags: ["React", "JavaScript","Framer Motion"],
-    imageUrl: TodoApp ,
+    tags: ["React", "JavaScript", "Framer Motion"],
+    imageUrl: TodoApp,
+    link: "",
   },
 ] as const;
 
